0d041ab3fac3c2ddc7231c4fac68318b6c319323
Lasha Morgoshia <xnagamer@live.com>
2024-09-19 19:00:45 +0400

    Add project files.

---
+++ Desk3500/Desk3500Api.cs
@@ -0,0 +1,171 @@
+﻿/*
+ * Author: Lasha Morgoshia
+ * Created On: 18.09.2024
+ * POS Terminal Integration Protocol for DESK 3500
+ *
+ */
+
+
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desk3500
+{
+    public class Desk3500Api
+    {
+        private readonly string baseUrl;
+        private readonly string licenseToken;
+        private readonly string alias;
+        private readonly string userName;
+        private readonly string password;
+        private string accessToken;
+
+        public Desk3500Api(string baseUrl, string licenseToken, string alias, string userName, string password)
+        {
+            this.baseUrl = baseUrl;
+            this.licenseToken = licenseToken;
+            this.alias = alias;
+            this.userName = userName;
+            this.password = password;
+        }
+
+        private async Task<HttpResponseMessage> SendPostRequest(string url, string contentJson)
+        {
+            using (var client = new HttpClient())
+            {
+                if (!string.IsNullOrEmpty(accessToken))
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
+                }
+
+                var content = new StringContent(contentJson, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return response;
+                }
+                else
+                {
+                    var error = await response.Content.ReadAsStringAsync();
+                    throw new Exception($"Error: {response.StatusCode}, {error}");
+                }
+            }
+        }
+
+        public async Task OpenPos()
+        {
+            var url = $"{baseUrl}/v105/openpos";
+            var requestJson = $@"
+        {{
+            ""licenseToken"": ""{licenseToken}"",
+            ""alias"": ""{alias}"",
+            ""userName"": ""{userName}"",
+            ""password"": ""{password}""
+        }}";
+
+            var response = await SendPostRequest(url, requestJson);
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("POS Connection Opened: " + result);
+            // Parse and store accessToken from the response
+            accessToken = "extracted-access-token"; // Update this based on response parsing
+        }
+
+        public async Task ClosePos()
+        {
+            var url = $"{baseUrl}/v105/closepos";
+            var requestJson = $@"
+        {{
+            ""accessToken"": ""{accessToken}""
+        }}";
+
+            var response = await SendPostRequest(url, requestJson);
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("POS Connection Closed: " + result);
+        }
+
+        public async Task AuthorizePayment(int amount, string documentNr, string currencyCode = "981", string panL4Digit = "")
+        {
+            var url = $"{baseUrl}/v105/executeposcmd";
+            var requestJson = $@"
+        {{
+            ""header"": {{
+                ""command"": ""AUTHORIZE""
+            }},
+            ""params"": {{
+                ""amount"": {amount},
+                ""cashBackAmount"": 0,
+                ""currencyCode"": ""{currencyCode}"",
+                ""documentNr"": ""{documentNr}"",
+                ""panL4Digit"": ""{panL4Digit}""
+            }}
+        }}";
+
+            var response = await SendPostRequest(url, requestJson);
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Payment Authorized: " + result);
+        }
+
+        public async Task RefundPayment(int amount, string documentNr, string stan, string rrn, string currencyCode = "981")
+        {
+            var url = $"{baseUrl}/v105/executeposcmd";
+            var requestJson = $@"
+        {{
+            ""header"": {{
+                ""command"": ""CREDIT""
+            }},
+            ""params"": {{
+                ""amount"": {amount},
+                ""currencyCode"": ""{currencyCode}"",
+                ""documentNr"": ""{documentNr}"",
+                ""STAN"": ""{stan}"",
+                ""RRN"": ""{rrn}""
+            }}
+        }}";
+
+            var response = await SendPostRequest(url, requestJson);
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Refund Processed: " + result);
+        }
+
+        public async Task VoidPayment(string operationId)
+        {
+            var url = $"{baseUrl}/v105/executeposcmd";
+            var requestJson = $@"
+        {{
+            ""header"": {{
+                ""command"": ""VOID""
+            }},
+            ""params"": {{
+                ""operationId"": ""{operationId}""
+            }}
+        }}";
+
+            var response = await SendPostRequest(url, requestJson);
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Transaction Voided: " + result);
+        }
+
+        public async Task GetTransactionStatus(string documentNr = "", string operationId = "", string cryptogram = "")
+        {
+            var url = $"{baseUrl}/v105/executeposcmd";
+            var requestJson = $@"
+        {{
+            ""header"": {{
+                ""command"": ""GETTRNSTATUS""
+            }},
+            ""params"": {{
+                {(string.IsNullOrEmpty(documentNr) ? "" : $@"""documentNr"": ""{documentNr}"",")}
+                {(string.IsNullOrEmpty(operationId) ? "" : $@"""operationId"": ""{operationId}"",")}
+                {(string.IsNullOrEmpty(cryptogram) ? "" : $@"""cryptogram"": ""{cryptogram}""")}
+            }}
+        }}";
+
+            var response = await SendPostRequest(url, requestJson);
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Transaction Status: " + result);
+        }
+    }
+}
---
+++ Desk3500/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Desk3500")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Desk3500")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7fdd8ebe-ada3-4e6e-9aa3-5943bf6c5713")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
