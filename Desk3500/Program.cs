fcd9727d3f7d1fa3f2d11c873a06edcb7c361e06
Lasha Morgoshia <xnagamer@live.com>
2024-10-07 14:44:57 +0400

    up

--- Desk3500/Program.cs
+++ Desk3500/Program.cs
@@ -5,6 +5,11 @@ namespace Desk3500
 {
     internal class Program
     {
+        static Desk3500Api api;
+        static string operatorId = "Cashier5-BOG-v3.4.5";
+        static string docNo = $"{DateTime.Now.Ticks}";
+        static string operationId { get; set; }
+
         static async Task Main(string[] args)
         {
             // პარამეტრები
@@ -14,69 +19,133 @@ namespace Desk3500
             var userName = "your-username";
             var password = "your-password";
 
-            // var docNo = $"{DateTime.Now.Date.Ticks}";
-            var docNo = $"{DateTime.Now.Ticks}";
-            var amount = 250;
-
             // API-ის ინიცირება
-            var desk3500Api = new Desk3500Api(baseUrl, licenseToken, alias, userName, password);
+            api = new Desk3500Api(baseUrl, licenseToken, alias, userName, password);
             
             try
             {
-                var operatorId = "Cashier5-BOG-v3.4.5";
+                // Init Token
+                await Auth();
 
-                // 1. POS_თან კავშირის დამყარება 638633376000000000
-                await desk3500Api.OpenPos();
+                // T01
+                await Purchase(9.99m);
 
-                //// 1.კავშირის დასასრული
-                //await desk3500Api.CloseDay(operatorId);
-                //await desk3500Api.WaitForDayCloseEvent();
-                //return;
+                // Dispose
+                await LogOut();
 
-                Console.WriteLine();
 
-                //await desk3500Api.UnlockAndWaitForCard(100, operatorId);
-
-                await desk3500Api.UnlockDevice(amount, operatorId);
-
-                await desk3500Api.WaitForCardEvent();
-
-                Console.WriteLine();
-
-                // 2. გადახდის ავტორიზაცია (თანხა არის თეთრებში. 1000 ნიშნავს 10.00 ლარს)
-                await desk3500Api.AuthorizePayment(amount, docNo);
-
-                var tranResult = await desk3500Api.WaitForCardEventResponse();
-
-                Console.WriteLine();
-
-                // await Task.Delay(3000);
-
-                // 3. სტატუსის გადამოწმება (optional)
-               //  var tranStatus = await desk3500Api.GetTransactionStatus(documentNr: docNo);
-
-                await desk3500Api.CloseDoc(docNo);
-
-                Console.WriteLine();
-
-                // 4. უკან დაბრუნება
-                // await desk3500Api.RefundPayment(1000, "123456", "123456", "933315462707");
-
-                // 5. ავტორიზებული ტრანზაქციის გაუქმება, რომელიც არ იყო დასრულებული
-                // await desk3500Api.VoidPayment("A0000000041010");
-
-                // 6. კავშირის დასასრული
-                await desk3500Api.LockDevice();
-
-                // 7. კავშირის დასასრული
-                await desk3500Api.ClosePos();
-
+                Console.WriteLine("payment completed.");
                 Console.ReadLine();
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error: " + ex.Message);
             }
+        }
+
+        /// <summary>
+        /// T01 - Purchase 9.99 - Ordinary Purchase - Approved
+        /// </summary>
+        /// <returns></returns>
+        private static async Task Purchase(decimal amount)
+        {
+            // convert amount
+            var amountInHundreds = (int)(amount * 100);
+
+            // pay
+            await Pay(amountInHundreds);
+
+            // authorize
+            await AuthorizeAmount(amountInHundreds);
+
+            // get status
+            var status = await GetStatus();
+            operationId = status.Properties.OperationId;
+
+            // close doc
+            await CloseDoc();
+        }
+
+        /// <summary>
+        /// auth and get token
+        /// </summary>
+        private static async Task Auth()
+        {
+            // 1. POS_თან კავშირის დამყარება 638633376000000000
+            await api.OpenPos();
+        }
+
+        private static async Task DayClose()
+        {
+            // 1.კავშირის დასასრული
+            await api.CloseDay(operatorId);
+        }
+
+        /// <summary>
+        /// Unlock and wait for card tap
+        /// </summary>
+        /// <param name="amount"></param>
+        private static async Task Pay(int amount)
+        {
+            await api.UnlockDevice(amount, operatorId);
+            await api.WaitForCardEvent();
+        }
+
+        /// <summary>
+        /// authorize amount
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        private static async Task AuthorizeAmount(int amount)
+        {
+            // 2. გადახდის ავტორიზაცია (თანხა არის თეთრებში. 1000 ნიშნავს 10.00 ლარს)
+            await api.AuthorizePayment(amount, docNo);
+        }
+
+        /// <summary>
+        /// get trn status
+        /// </summary>
+        /// <returns></returns>
+        private static async Task<TransactionStatus> GetStatus()
+        {
+            return await api.WaitForCardEventResponse();
+
+            // 3. სტატუსის გადამოწმება (optional)
+            //  var tranStatus = await desk3500Api.GetTransactionStatus(documentNr: docNo);
+        }
+
+        /// <summary>
+        /// close doc and lock device
+        /// </summary>
+        /// <returns></returns>
+        private static async Task CloseDoc()
+        {
+            // დოკუმენტის დახურვა
+            await api.CloseDoc(docNo);
+            // 6. კავშირის დასასრული
+            await api.LockDevice();
+        }
+
+        private static async Task Refund(int amount)
+        {
+            // 4. უკან დაბრუნება
+            await api.RefundPayment(amount, docNo, "123456", "933315462707");
+        }
+
+        private static async Task Void()
+        {
+            // 5. ავტორიზებული ტრანზაქციის გაუქმება, რომელიც არ იყო დასრულებული
+            await api.VoidPayment(operationId);
+        }
+
+        /// <summary>
+        /// diactivate token
+        /// </summary>
+        /// <returns></returns>
+        private static async Task LogOut()
+        {
+            // 7. კავშირის დასასრული
+            await api.ClosePos();
         }
     }
 }
